2682ec4ed10929ea9b0ee60722d7efb828d315bb
AnilEnsari <anilensari@gmail.com>
2023-08-09 00:55:39 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,32 @@ function App() {
     { isim: "Veli", email: "veli@example.com", rol: "Backend Developer" },
   ];
   const [teamMembers, setTeamMembers] = useState(myMembers);
+  const [duzenlenecekUye, setDuzenlenecekUye] = useState({
+    isim: "",
+    email: "",
+    rol: "",
+  });
   const addMembers = (member) => {
     setTeamMembers([...teamMembers, member]);
   };
   return (
-    <div>
+    <div className="App">
       <h1>Team Members</h1>
       <ul>
         {teamMembers.map((member, index) => (
           <li key={index}>
             {member.isim}-{member.email}-{member.rol}
+            <button onClick={() => setDuzenlenecekUye(member)}>Düzenle</button>
+            {console.log(duzenlenecekUye)}
           </li>
         ))}
       </ul>
-      <Form addMembers={addMembers} teamMembers={teamMembers} />
+      <Form
+        addMembers={addMembers}
+        teamMembers={teamMembers}
+        duzenlenecekUye={duzenlenecekUye}
+        setDuzenlenecekUye={setDuzenlenecekUye}
+      />
     </div>
   );
 }
